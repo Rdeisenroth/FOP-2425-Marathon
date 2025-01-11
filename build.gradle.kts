8154901a1ad9783b504dfd62255dd014b413da08
Rdeisenroth <rdeisenroth@gmail.com>
2025-01-11 16:12:33 +0100

    Squashed 'solution/H07/' content from commit d9306fc
    git-subtree-dir: solution/H07
    git-subtree-split: d9306fc9005e1c340f81f2ea6c5fb9f437372207

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.algomate)
+    alias(libs.plugins.style)
+}
+
+version = file("version").readLines().first()
+
+exercise {
+    assignmentId.set("h07")
+}
+
+submission {
+    // ACHTUNG!
+    // Setzen Sie im folgenden Bereich Ihre TU-ID (NICHT Ihre Matrikelnummer!), Ihren Nachnamen und Ihren Vornamen
+    // in Anführungszeichen (z.B. "ab12cdef" für Ihre TU-ID) ein!
+    // BEISPIEL:
+    // studentId = "ab12cdef"
+    // firstName = "sol_first"
+    // lastName = "sol_last"
+    studentId = "ab12cdef"
+    firstName = "sol_first"
+    lastName = "sol_last"
+
+    // Optionally require own tests for mainBuildSubmission task. Default is false
+    requireTests = false
+}
+
+jagr {
+    graders {
+        val graderPublic by getting
+        val graderPrivate by creating {
+            parent(graderPublic)
+            graderName.set("FOP-2425-H07-Private")
+            rubricProviderName.set("h07.H07_RubricProvider")
+        }
+    }
+}
